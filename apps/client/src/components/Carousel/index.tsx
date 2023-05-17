422086e7e9a83dfc3ad47da213c33007f837ca7f
Felipe Viana <felipetiburcioviana@gmail.com>
2023-05-17 14:30:32 -0700

    Update index.tsx

--- apps/client/src/components/Carousel/index.tsx
+++ apps/client/src/components/Carousel/index.tsx
@@ -11,7 +11,7 @@ export function Carousel(  props : any ){
     {/* <!-- Carousel wrapper --> */}
     <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
          {/* <!-- Item 1 --> */}
-            { props.urls.map( url => (
+            { props.map( url => (
             <div className="hidden duration-700 ease-in-out" data-carousel-item>
                 <Image src={url} className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
             </div>
