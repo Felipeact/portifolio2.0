077eaccccd6a64d446d9203fbb04307fe372495c
Felipe Viana <felipetiburcioviana@gmail.com>
2023-05-18 11:02:26 -0700

    Update page.tsx

--- apps/client/src/app/projects/[id]/page.tsx
+++ apps/client/src/app/projects/[id]/page.tsx
@@ -102,7 +102,7 @@ export default function ProjectsId({
             {/* <!-- Carousel wrapper --> */}
             <       div className="relative h-56 overflow-hidden rounded-lg md:h-96">
                 {/* <!-- Item 1 --> */}
-                {project?.images.map((url : string) => (
+                {project?.technologies.map(url => (
                 <div className="hidden duration-700 ease-in-out" data-carousel-item>
                     <Image src={url} className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
                 </div>
