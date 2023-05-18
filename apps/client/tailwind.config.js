b16d4ecc1aa11320fcf86029524fdf0cb2d2558d
Felipe Viana <felipetiburcioviana@gmail.com>
2023-05-18 14:01:40 -0700

    Update tailwind.config.js

--- apps/client/tailwind.config.js
+++ apps/client/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./node_modules/flowbite-react/**/*.{js,tsx,ts}",
     "./app/**/*.{js,ts,jsx,tsx}", // Note the addition of the `app` directory.
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
@@ -53,6 +54,7 @@ module.exports = {
   },
   plugins: [
     require('@tailwindcss/forms'),
-    require('tailwind-scrollbar')
+    require('tailwind-scrollbar'),
+    require("flowbite/plugin")
   ],
 }
