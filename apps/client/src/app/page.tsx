b55006eaf581d981cfaaa7539b54a6300c7a36d3
Felipe Viana <felipetiburcioviana@gmail.com>
2023-10-20 09:32:05 -0700

    Update page.tsx

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -32,7 +32,7 @@ export default function Home() {
           <div className="mt-4">
             <h5> Felipe Viana </h5>
             <p> Gameplay Programmer </p>
-            <p className=""m-10">https://www.youtube.com/channel/UCMYDSj6uAmnq4C_WvVF9C7g</p>
+            <Link className=""m-10" to="https://www.youtube.com/channel/UCMYDSj6uAmnq4C_WvVF9C7g">My Channel</Link>
           </div>
         </div>
         
