4dbebed9820cbe3ae97d359a0e3bdb0cc9da363a
Felipe Viana <felipetiburcioviana@gmail.com>
2023-10-24 10:06:14 -0700

    Create Movies.tsx

---
+++ apps/client/src/components/Movies.tsx
@@ -0,0 +1,30 @@
+import Nav from './Nav';
+import NavItem from './NavItem';
+import List from './List';
+import ListItem from './ListItem';
+
+interface Movie {
+  id: number;
+  // Add more properties if needed
+}
+
+interface MoviesProps {
+  movies: Movie[];
+}
+
+export function Movies({ movies } : MoviesProps) {
+  return (
+    <div className="divide-y divide-slate-100">
+      <Nav>
+        <NavItem href="/new" isActive>New Releases</NavItem>
+        <NavItem href="/top">Top Rated</NavItem>
+        <NavItem href="/picks">Vincent’s Picks</NavItem>
+      </Nav>
+      <List>
+        {movies.map((movie) => (
+          <ListItem key={movie.id} movie={movie} />
+        ))}
+      </List>
+    </div>
+  );
+}
