f29dec3eb1cb3a569c5a7eadf1266e9a6cecf146
Felipe Viana <felipetiburcioviana@gmail.com>
2023-10-24 09:44:04 -0700

    Update Nav.tsx

--- apps/client/src/components/Nav.tsx
+++ apps/client/src/components/Nav.tsx
@@ -1,4 +1,9 @@
-export default function Nav({ children } : any ) {
+interface NavProps {
+  children: ReactNode;
+}
+
+
+export default function Nav({ children } : NavProps ) {
   return (
     <nav className="py-4 px-6 text-sm font-medium">
       <ul className="flex space-x-3">
