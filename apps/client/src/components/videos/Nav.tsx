c2d15eb82524a493b8520b4b1396e1b9744196a9
Felipe Viana <felipetiburcioviana@gmail.com>
2023-12-11 09:14:23 -0800

    Update Nav.tsx

--- apps/client/src/components/videos/Nav.tsx
+++ apps/client/src/components/videos/Nav.tsx
@@ -7,7 +7,7 @@ interface NavProps {
 
 export default function Nav({ children } : NavProps ) {
   return (
-    <nav className="py-4 px-6 text-sm font-medium mt-16 md:mt-[7rem] ">
+    <nav className="py-4 px-6 text-sm font-medium mt-16 md:mt-[6rem] ">
       <ul className="flex space-x-3">
         {children}
       </ul>
