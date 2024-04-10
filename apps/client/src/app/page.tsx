13d9b21239575a9bb876e4a4444515c4a2ba8620
Felipe Viana <felipetiburcioviana@gmail.com>
2024-04-10 10:50:02 -0700

    Update page.tsx

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,13 +1,13 @@
 'use client'
 import Image from 'next/image'
 import Movies from '../components/videos'
-
-import MyPhoto from "../../public/myphoto.jpg"
 import { Outfit, Roboto } from 'next/font/google'
 import { useState, useEffect } from 'react';
 import { youtubeApi } from '../services/youtubeApi';
 import Button3D from '../components/Button3D';
+import LoadingIndicator from '../components/LoadingIndicator'; // Import your loading indicator component
 
+import MyPhoto from "../../public/myphoto.jpg"
 
 const outfit = Outfit({
   subsets: ['latin'],
@@ -20,15 +20,28 @@ const roboto = Roboto({
   weight: '400'
 })
 
-
 export default function Home() {
-
+  const [loading, setLoading] = useState(true); // Define a loading state
   
+  useEffect(() => {
+    // Simulate loading process
+    const timer = setTimeout(() => {
+      setLoading(false); // Set loading to false after a certain time (simulating loading completion)
+    }, 3000); // Adjust the timeout according to your loading time
 
+    // Clear the timeout when the component unmounts
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
     <main className="min-h-screen md:min-h-[calc(100vh_-_5rem)] mx-auto lg:max-w-[90%] md:overflow-hidden ">
-
+      {/* Render loading indicator when loading is true */}
+      {loading && 
+        <h1>
+          Loading
+        </h1>
+      }
+      
       <div className=' flex lg:flex-row flex-col items-center justify-between mx-auto p-8 '>
         <div className="p-4 md:mt-[4rem]">
           <div>
@@ -44,30 +57,11 @@ export default function Home() {
             <p>Passionate to help you solve problems , build products or grow an existing project .</p>
           </div>
             
-
             <Button3D />
         </div>
         
         <Movies />
       </div>
-
-{/*         <div>
-          <h2 className={`${outfit.className} text-center font-bold text-5xl mb-20`}>My Technologies.</h2>
-
-          <div className="grid grid-cols-3 w-[90%] md:w-2/4 justify-items-center mx-auto sm:justify-center sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-5 gap-4 pb-8 ">
-              <img src='https://upload-images-projects.s3.us-east-1.amazonaws.com/b1a7f0a21cec06a11f5dc3f42097feb3-typescript.svg' className="filter brightness-0 h-12 mt-8 invert-[0.6] hover:invert-[1] cursor-pointer"/>
-              <img src='https://upload-images-projects.s3.amazonaws.com/737d48b4c831ae3ce167b5af094703c8-tailwindcss.svg' className="filter brightness-0 h-12 mt-8 invert-[0.6] hover:invert-[1] cursor-pointer"/>
-              <img src='https://upload-images-projects.s3.amazonaws.com/afe3777d886669e33bb6a0a700bc9fce-nodejs-icon.svg' className="filter brightness-0 h-12 mt-8 invert-[0.6] hover:invert-[1] cursor-pointer"/>
-              <img src='https://upload-images-projects.s3.amazonaws.com/be1b286637e169a85c64d1aae3d0d38a-vitejs.svg' className="filter brightness-0 h-12 mt-8 invert-[0.6] hover:invert-[1] cursor-pointer"/>
-              <img src='https://upload-images-projects.s3.amazonaws.com/5c73baaef633e851f50a3d738743ae5a-graphql.svg' className="filter brightness-0 h-12 mt-8 invert-[0.6] hover:invert-[1] cursor-pointer"/>
-              <img src='https://upload-images-projects.s3.amazonaws.com/d77b2c1ee7596b7efbefbdb6963be115-react.svg' className="filter brightness-0 h-12 mt-8 invert-[0.6] hover:invert-[1] cursor-pointer"/>
-              <img src='https://upload-images-projects.s3.amazonaws.com/03555ac8dbd63fd489a2a25cd16c2f1e-Fauna-Logo.svg' className="filter brightness-0 h-12 mt-8 invert-[0.6] hover:invert-[1] cursor-pointer"/>
-              <img src='https://upload-images-projects.s3.amazonaws.com/815bf9e9fd6eb68d9ff225fb395fe0de-next-js.svg' className="filter brightness-0 h-12 mt-8 invert-[0.6] hover:invert-[1] cursor-pointer"/>
-              <img src='https://upload-images-projects.s3.amazonaws.com/45c0cd42a899239b7bd70506672ee43f-stripe.svg' className="filter brightness-0 h-12 mt-8 invert-[0.6] hover:invert-[1] cursor-pointer"/>
-              <img src='https://upload-images-projects.s3.amazonaws.com/9c9b9bce8c9d1a3394c6331366b1a893-kafka.svg' className="filter brightness-0 h-12 mt-8 invert-[0.6] hover:invert-[1] cursor-pointer"/>
-          </div>
-        </div> */}
-
-    </main >
+    </main>
   )
 }
