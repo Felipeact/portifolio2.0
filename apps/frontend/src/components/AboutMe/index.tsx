4ca2daee9d9aec3206157d0461b1a4a1531c2f46
Felipe Viana <felipetiburcioviana@gmail.com>
2025-04-20 21:29:55 +0000

    debug aboutme component

--- apps/frontend/src/components/AboutMe/index.tsx
+++ apps/frontend/src/components/AboutMe/index.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
-import Image from 'next/image'
-import { Outfit } from 'next/font/google'
+import Image from "next/image"
+import { Outfit } from "next/font/google"
 
 
 import MyPhoto from "../../../public/myphoto.png"
@@ -8,8 +8,8 @@ import MyPhoto from "../../../public/myphoto.png"
 
 
 const outfit = Outfit({
-    subsets: ['latin'],
-    display: 'swap',
+    subsets: ["latin"],
+    display: "swap",
 })
 
 interface AboutMeProps {
@@ -22,15 +22,15 @@ export function AboutMe({ title }: AboutMeProps) {
     return (
         <>
             {/* About me */}
-            <div className=' mt-[6rem] lg:flex lg:flex-wrap lg:content-center'>
+            <div className=" mt-[6rem] lg:flex lg:flex-wrap lg:content-center">
                 {/* Title and Introduction */}
-                <div className='lg:w-[46%]'>
+                <div className="lg:w-[46%]">
                     {/* Title */}
                     <h2 className={`${outfit.className} text-center text-4xl my-12 text-purple-500 font-bold`}>{title}</h2>
 
                     {/* Short Introduction  */}
-                    <p className='p-4 text-xl text-center'>
-                        I'm Felipe Viana, a passionate gameplay programmer with a relentless drive to craft immersive and captivating gaming experiences.
+                    <p className="p-4 text-xl text-center">
+                        I"m Felipe Viana, a passionate gameplay programmer with a relentless drive to craft immersive and captivating gaming experiences.
                         With a fervent love for both technology and creativity, I thrive in the dynamic world where code meets artistry,
                         bringing virtual worlds to life one line at a time.
                     </p>
@@ -50,18 +50,18 @@ export function AboutMe({ title }: AboutMeProps) {
                 </div>
 
                 {/* mMore about me */}
-                <div className='p-4'>
+                <div className="p-4">
 
-                    <p className='text-xl text-center'>
+                    <p className="text-xl text-center">
                         Driven by a profound fascination for interactive storytelling and game mechanics,
-                        I've embarked on a journey to master the craft of gameplay programming.
-                        With a keen eye for detail and a dedication to pushing the boundaries of what's possible,
+                        I"ve embarked on a journey to master the craft of gameplay programming.
+                        With a keen eye for detail and a dedication to pushing the boundaries of what"s possible,
                         I constantly seek to innovate and elevate gameplay experiences to new heights.
                     </p>
 
-                    <p className='text-xl text-center p-4'>
+                    <p className="text-xl text-center p-4">
                         In every project I undertake, my goal is clear: to create experiences that not only entertain but resonate,
-                        leaving players with memories that linger long after they've put down the controller.
+                        leaving players with memories that linger long after they"ve put down the controller.
                         Join me on this journey as we delve into the boundless realms of possibility, where imagination knows no bounds and gameplay knows no limits.
                     </p>
 
